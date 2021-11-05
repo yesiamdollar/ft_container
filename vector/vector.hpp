bf4f1143694cdf19b7bd3962dd426e2e5d6f4fd4
yesiamdollar <achrafboutahri17@gmail.com>
2021-11-05 12:55:58 +0100

    working on range

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -13,14 +13,18 @@ namespace ft{
 	template <typename T, class Alloc = std::allocator<T> >
 	class vector{
 		private:
-			typedef				T								value_type;
-			typedef				Alloc							allocator_type;
-			typedef	typename	allocator_type::reference		reference;
-			typedef	typename	allocator_type::const_reference	const_reference;
-			typedef	typename	allocator_type::pointer			pointer;
-			typedef	typename	allocator_type::const_pointer	const_pointer;
-			typedef	typename	ft::iterator< vector<T> >			iterator;
-			typedef				size_t							size_type;
+			typedef				T											value_type;
+			typedef				Alloc										allocator_type;
+			typedef	typename	allocator_type::reference					reference;
+			typedef	typename	allocator_type::const_reference				const_reference;
+			typedef	typename	allocator_type::pointer						pointer;
+			typedef	typename	allocator_type::const_pointer				const_pointer;
+			typedef				ft::iterator< value_type >					iterator;
+			typedef				ft::iterator< const value_type>				const_iterator;
+			typedef				ft::reverse_iterator< value_type >			reverse_iterator;
+			typedef				ft::reverse_iterator< const value_type >	const_reverse_iterator;
+			typedef				std::ptrdiff_t								difference_type;
+			typedef				size_t										size_type;
 
 			value_type											*arr;
 			size_type											size;
@@ -46,7 +50,9 @@ namespace ft{
 				arr = allocateNfill(count, val, alloc);
 			}
 
-			
+			vector(  ){
+
+			}
 
 			iterator begin(){
 				return iterator(arr);
