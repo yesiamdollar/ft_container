31a6ae9cc507bff2f673ee376b1ea589d43a27ea
Achraf Boutahri <aboutahr@e2r1p3.1337.ma>
2021-11-06 17:12:22 +0100

    workin on operator=

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@
 
 
 int main(){
-	
+	std::vector<int> v(3, 2);
+
+	std::cout << std::hex << &v << std::endl;
+	v.reserve(10);
+	std::cout << v.capacity() << std::endl;
+	std::cout << std::hex << &v << std::endl;
+	v.push_back(33);
+	for (std::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+		std::cout << &it << '\n';
 	return 0;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -33,48 +33,97 @@ namespace ft{
 			size_type											_capacity;
 			allocator_type										_alloc;
 
-			value_type*		allocateNfill(size_type n, const value_type& val,
-					const allocator_type& alloc){
-				value_type*	tmp = alloc.allocate(n);
-				for (size_type i = 0; i < n; i++)
-					tmp = val;
-				return tmp;
-			};
 		public:
+			/* Member functions */
 			vector(const allocator_type& alloc = allocator_type()){
 				_size = 0;
 				_capacity = 0;
+				_alloc = alloc;
 				_arr = NULL;
 			}
 			vector(size_type count, const value_type& val = value_type(),
 					const allocator_type& alloc = allocator_type()){
 				_size = count;
 				_capacity = count;
-				_arr = allocateNfill(count, val, alloc);
+				_alloc = alloc;
+				_arr = _alloc.allocate(count);
+				for (size_type z = 0; z < _size; z++)
+					_arr[z] = val;
 			}
-			<template InputIt>
+			template <InputIt>
 			vector( InputIt first, InputIt last, const allocator_type& alloc = allocator_type(),
 					typename ft::enable_if< !ft::is_integral<InputIt>, InputIt >::value = InputIt()){
 					difference_type len = std::distance(first, last);
 					_arr = alloc.allocate(len);
+					_alloc = alloc;
 					_capacity = len;
-					for (InputIt it = first, int z = 0; first != last; first++, z++){
-						alloc.construct(_arr + z, *first );
+					_size = 0;
+					for (InputIt it = first, int z = 0; it != last; it++, z++){
+						_alloc.construct(_arr + z, *it );
 						_size++;
 					}
 			}
-
+			vector( const vector& other ){
+				operator=(other);
+			}
 			vector&	operator=(const vector& other){
-				
+				if (this != &other){
+					this._capacity = other.capacity();
+					this._size = other.size();
+					this._alloc = other.get_allocator();
+					_arr = _alloc.allocate(_capacity);
+					assign(other.begin(), other.end());
+					other._alloc.d
+				}
+				return *this;
+			}
+			void	assign( size_type count, const value_type& value ){
+				_size = 0;
+				for(int z = 0; z < count ; z++, _size++)
+					arr[z] = value;
+			}
+			template <InputIt>
+			void	assign( InputIt first, InputIt last ){
+				// difference_type len = std::distance(first, last)
+				_size = 0;
+				for(InputIt it = first; it != last; it++, _size++ )
+					_alloc.construct(_arr + _size, *it);
+
 			}
 
+			allocator_type get_allocator() const{
+				return _alloc;
+			};
+
+			/* end Member functions */
+
+			/* Iterators */
 			iterator begin(){
 				return iterator(arr);
 			}
-
 			iterator end(){
 				return iterator(arr + size);
+			}	
+			/* end Iterators */
+			
+			
+			/* Capacity */
+			bool	empty() const {
+				return (begin() == end());
 			}
+			size_type	size() const {
+				return _size;
+			}
+			size_type	max_size() const{
+				return _alloc.max_size();
+			}
+			void	reserve( size_type new_cap ){
+				(void)new_cap;
+			}
+			size_type	capacity() const {
+				return _capaity;
+			}
+			/* end Capacity */
 	};
 }
 
