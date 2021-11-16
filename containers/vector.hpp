adcca2f5873f016936b45e28310a24b6437fc867
yesiamdollar <achrafboutahri17@gmail.com>
2021-11-16 10:17:51 +0100

    swap Vector test

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -329,7 +329,6 @@ namespace ft{
 			void	swap(Vector& other){
 				size_type	_sz = other._size, _cp = other._capacity;
 				T* new_arr = other._arr;
-				// allocator_type new_alloc = other._alloc;
 
 				other._size = this->_size;
 				this->_size = _sz;
@@ -339,13 +338,6 @@ namespace ft{
 				
 				other._arr = this->_arr;
 				this->_arr = new_arr;
-
-				// other._alloc = new_alloc;
-				// this->_alloc = new_alloc;
-				// std::swap(this->_size, other._size);
-				// std::swap(this->_capacity, other._capacity);
-				// std::swap(this->_arr, other._arr);
-				// std::swap(this->_alloc, other._alloc);
 			}
 
 			void	clear(){
