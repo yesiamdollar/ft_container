e0dd41303cca3c81731e82ba6905c11d24434899
yesiamdollar <achrafboutahri17@gmail.com>
2021-11-16 10:09:06 +0100

    insert Vector fixed

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -243,6 +243,7 @@ namespace ft{
 					_alloc.construct(_arr + i + n, _arr[i]);
 				for (size_type i = 0; i < n; i++)
 					_alloc.construct( _arr + dis + i, value);
+				_size += n;
 			}
 
 			template<class InputIt>
@@ -260,8 +261,9 @@ namespace ft{
 				}
 				for (difference_type i = _size - 1; i >= dis; i--)
 					_alloc.construct(_arr + i + n, _arr[i]);
-				for (size_type i = 0; i < n; i++, _size++)
+				for (size_type i = 0; i < n; i++)
 					_alloc.construct(_arr + dis + i, *first);
+				_size += n;
 			}
 
 			
